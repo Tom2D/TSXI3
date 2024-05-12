2832e8b27803d672fef10e0910008b52afc0051e
TomDD <scarletinne@hotmail.com>
2024-05-12 03:40:37 -0400

    very simple version working

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,5 @@
-//import {API_PLAYGROUND_COUNT}  from "@tsxinsider/shared";
+import {API_PLAYGROUND_COUNT}  from "@tsxinsider/shared";
 import {meaningOfLife} from "@nighttrax/foo";
 
 console.log(meaningOfLife);
+console.log(API_PLAYGROUND_COUNT);
