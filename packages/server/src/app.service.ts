3d02152b33bc1b67232439925f97b5b067fc112b
TomDD <scarletinne@hotmail.com>
2024-05-12 04:51:31 -0400

    nest js working

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import {API_PLAYGROUND_COUNT} from "@tsxinsider/shared";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello ' + API_PLAYGROUND_COUNT;
   }
 }
