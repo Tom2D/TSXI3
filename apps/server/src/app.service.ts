86b612464187fc41349c3513cecf05de03ff8be5
TomDD <scarletinne@hotmail.com>
2024-05-13 21:31:32 -0400

    Major cleanup Removed every example from template Moved server from packages to apps Updated deprecated dependencies

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import {API_PLAYGROUND_COUNT} from "@tsxinsider/shared";
+import { API_PLAYGROUND_COUNT } from '@tsxinsider/shared';
 
 @Injectable()
 export class AppService {
--- packages/server/src/index.ts
+++
@@ -1,5 +0,0 @@
-import {API_PLAYGROUND_COUNT}  from "@tsxinsider/shared";
-import {meaningOfLife} from "@nighttrax/foo";
-
-console.log(meaningOfLife);
-console.log(API_PLAYGROUND_COUNT);
