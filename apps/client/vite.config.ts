e410f52e099cb0b36d6f64f70c22beb5424d1f51
TomDD <scarletinne@hotmail.com>
2024-05-18 01:26:29 -0400

    Fixed dependency client -> shared temp: added datepicker to client

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { CLIENT_PORT } from "@tsxinsider/shared/src/constants/app-config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: CLIENT_PORT,
+  },
   plugins: [react()],
-})
+});
