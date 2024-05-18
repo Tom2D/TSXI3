67ca35d0f3dabe5bba249ccdd6ae2898790f4ca7
TomDD <scarletinne@hotmail.com>
2024-05-18 04:27:19 -0400

    Revert "essayer de partager prisma ac tool custom d'un rando, MARCHE PAS"
    This reverts commit c74eda698a8282687d9829eaccf1bb6752e1130c.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,7 +1,3 @@
 export * from "./constants/app-config";
 
-//import tickers from './prisma-types'
-
 export const API_PLAYGROUND_COUNT: string = "/api/playground/count";
-
-//export const the_tickers = tickers
