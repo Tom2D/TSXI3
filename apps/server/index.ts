4e90b3a085cd39f5c32f4634cfb81c2f9d2127ea
TomDD <scarletinne@hotmail.com>
2024-05-19 01:56:02 -0400

    share transactions work (dirty gotta clean) mais webstorm donne encore une erreur

---
+++ apps/server/index.ts
@@ -0,0 +1,3 @@
+export const TEST_EXPORT = 'cmonnnn';
+
+export * from '@prisma/client';
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./constants/app-config";
 
 export const API_PLAYGROUND_COUNT: string = "/api/playground/count";
+
+//export { transactions } from '@tsxinsider/server'
+
+//export type transactions = PrismaClient[transactions];
