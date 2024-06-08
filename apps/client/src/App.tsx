eee6a271c810bdc97efc4bc080af472cb96537c8
TomDD <scarletinne@hotmail.com>
2024-06-08 02:27:29 -0400

    - Retrait colonnes superflues - Permettre de resize les colonnes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -5,9 +5,15 @@ import "react-datepicker/dist/react-datepicker.css";
 import { SERVER_AUTHORITY } from "@tsxinsider/shared";
 import { transactions, trnnatures } from "./prisma-types";
 import { FormatDateUTC } from "./util/date";
-import Select, { MultiValue, StylesConfig } from 'react-select';
-import DataEditor, { DataEditorProps, GridCell, GridCellKind, GridColumn, Item } from "@glideapps/glide-data-grid";
-import "@glideapps/glide-data-grid/dist/index.css"
+import Select, { MultiValue, StylesConfig } from "react-select";
+import DataEditor, {
+  DataEditorProps,
+  GridCell,
+  GridCellKind,
+  GridColumn,
+  Item,
+} from "@glideapps/glide-data-grid";
+import "@glideapps/glide-data-grid/dist/index.css";
 import { darkTheme } from "./grid/dark-theme.tsx";
 
 const DEFAULT_TRN_NATURE = 10;
@@ -16,69 +22,75 @@ const customStyles: StylesConfig<trnnatures> = {
   control: (provided) => ({
     ...provided,
     width: 300,
-    backgroundColor: '#333',  // Dark background for the control
-    color: '#fff',            // White text
-    border: '1px solid #444', // Border color to match dark theme
+    backgroundColor: "#333", // Dark background for the control
+    color: "#fff", // White text
+    border: "1px solid #444", // Border color to match dark theme
   }),
   menu: (provided) => ({
     ...provided,
-    backgroundColor: '#333', // Dark background for the menu
-    color: '#fff',           // White text
+    backgroundColor: "#333", // Dark background for the menu
+    color: "#fff", // White text
   }),
   multiValue: (provided) => ({
     ...provided,
-    backgroundColor: '#555', // Dark background for selected options
+    backgroundColor: "#555", // Dark background for selected options
   }),
   multiValueLabel: (provided) => ({
     ...provided,
-    color: '#fff', // White text for selected options
+    color: "#fff", // White text for selected options
   }),
   option: (provided, state) => ({
     ...provided,
-    backgroundColor: state.isSelected ? '#555' : state.isFocused ? '#444' : '#333',
-    color: '#fff', // White text for options
-    '&:hover': {
-      backgroundColor: '#444', // Background color on hover
-    }
+    backgroundColor: state.isSelected
+      ? "#555"
+      : state.isFocused
+      ? "#444"
+      : "#333",
+    color: "#fff", // White text for options
+    "&:hover": {
+      backgroundColor: "#444", // Background color on hover
+    },
   }),
   singleValue: (provided) => ({
     ...provided,
-    color: '#fff', // White text for single value
+    color: "#fff", // White text for single value
   }),
   input: (provided) => ({
     ...provided,
-    color: '#fff', // White text for input
+    color: "#fff", // White text for input
   }),
 };
 
-const columns: GridColumn[] = [
-  { id: "id", title: "ID" },
-  { id: "sediId", title: "SEDI ID" },
-  { id: "insiderId", title: "Insider ID" },
-  { id: "issuerId", title: "Issuer ID" },
-  { id: "securityId", title: "Security ID" },
-  { id: "trnFlagId", title: "Transaction Flag ID" },
-  { id: "trnDate", title: "Transaction Date" },
-  { id: "filingDate", title: "Filing Date" },
-  { id: "ownershipType", title: "Ownership Type" },
-  { id: "trnNatureCode", title: "Transaction Nature Code" },
-  { id: "nb", title: "Number" },
-  { id: "price", title: "Price" },
-  { id: "priceCurrencyId", title: "Price Currency ID" },
-  { id: "closingBalance", title: "Closing Balance" },
-  { id: "calculatedBalance", title: "Calculated Balance" },
-  { id: "underlyingSecurityId", title: "Underlying Security ID" },
-  { id: "GeneralRemarks", title: "General Remarks" },
+const initialColumns: GridColumn[] = [
+  { id: "trnFlagId", title: "Flag", width: 50 },
+  { id: "filingDate", title: "Filing Date", width: 100 },
+  { id: "trnDate", title: "Trade Date", width: 100 },
+  { id: "issuerId", title: "Issuer ID", width: 100 },
+  { id: "insiderId", title: "Insider ID", width: 100 },
+  { id: "trnNatureCode", title: "Transaction Nature Code", width: 100 },
+  { id: "price", title: "Price", width: 90 },
+  { id: "nb", title: "Number", width: 100 },
+  { id: "securityId", title: "Security Type", width: 100 },
+  { id: "closingBalance", title: "Closing Balance", width: 100 },
+  { id: "ownershipType", title: "Ownership", width: 125 },
+  { id: "GeneralRemarks", title: "General Remarks", width: 200, grow: 1 },
 ];
 
 function App() {
   const [trns, setTransactions] = useState<transactions[]>([]);
-  const [trnNatureOptions, setTrnNatureOptions] = useState<{ value: number; label: string }[]>([]);
-  const [selectedTrnNatures, setSelectedTrnNatures] = useState<number[]>([DEFAULT_TRN_NATURE]);
-  const [startDate, setStartDate] = useState<Date | null>(new Date("2021-01-22"));
+  const [trnNatureOptions, setTrnNatureOptions] = useState<
+    { value: number; label: string }[]
+  >([]);
+  const [selectedTrnNatures, setSelectedTrnNatures] = useState<number[]>([
+    DEFAULT_TRN_NATURE,
+  ]);
+  const [startDate, setStartDate] = useState<Date | null>(
+    new Date("2021-01-22")
+  );
   const [endDate, setEndDate] = useState<Date | null>(new Date("2021-01-22"));
   const [limit, setLimit] = useState<number>(10);
   const [page, setPage] = useState<number>(1);
+  const [columns, setColumns] = useState<GridColumn[]>(initialColumns);
 
   useEffect(() => {
     fetchTrnNatures();
@@ -113,7 +125,7 @@ function App() {
       const endDateStr = FormatDateUTC(endDate);
       const trnNatureCodes = selectedTrnNatures.join(",");
       const response = await fetch(
-          `${SERVER_AUTHORITY}/transactions?beginFilingDate=${startDateStr}&endFilingDate=${endDateStr}&limit=${limit}&page=${pageNumber}&trnNatureCodes=${trnNatureCodes}`
+        `${SERVER_AUTHORITY}/transactions?beginFilingDate=${startDateStr}&endFilingDate=${endDateStr}&limit=${limit}&page=${pageNumber}&trnNatureCodes=${trnNatureCodes}`
       );
 
       if (response.ok) {
@@ -128,35 +140,51 @@ function App() {
     }
   };
 
-  const handleTrnNatureChange = (selectedOptions: MultiValue<{ value: number; label: string }>) => {
-    const selectedCodes = selectedOptions ? selectedOptions.map((option) => option.value) : [];
+  const handleTrnNatureChange = (
+    selectedOptions: MultiValue<{ value: number; label: string }>
+  ) => {
+    const selectedCodes = selectedOptions
+      ? selectedOptions.map((option) => option.value)
+      : [];
     setSelectedTrnNatures(selectedCodes);
   };
 
   const getData = useCallback(
-      ([col, row]: Item): GridCell => {
-        const transaction = trns[row];
-        const column = columns[col];
-        const value = transaction[column.id as keyof transactions];
+    ([col, row]: Item): GridCell => {
+      const transaction = trns[row];
+      const column = columns[col];
+      const value = transaction[column.id as keyof transactions];
 
-        if (column.id === "trnDate" || column.id === "filingDate") {
-          return {
-            kind: GridCellKind.Text,
-            allowOverlay: false,
-            displayData: new Date(value as string).toLocaleDateString(),
-            data: new Date(value as string).toLocaleDateString(),
-          };
-        }
-
+      if (column.id === "trnDate" || column.id === "filingDate") {
         return {
           kind: GridCellKind.Text,
           allowOverlay: false,
-          displayData: String(value),
-          data: String(value),
+          displayData: new Date(value as string).toLocaleDateString(),
+          data: new Date(value as string).toLocaleDateString(),
         };
-      },
-      [trns]
+      }
+
+      return {
+        kind: GridCellKind.Text,
+        allowOverlay: false,
+        displayData: String(value),
+        data: String(value),
+      };
+    },
+    [trns, columns]
   );
+
+  const onColumnResize = useCallback((column: GridColumn, newSize: number) => {
+    setColumns((prevColsMap: GridColumn[]) => {
+      const index = columns.findIndex((ci) => ci.title === column.title);
+      const newArray = [...prevColsMap];
+      newArray.splice(index, 1, {
+        ...prevColsMap[index],
+        width: newSize,
+      });
+      return newArray;
+    });
+  }, [columns]);
 
   const gridProps: Partial<DataEditorProps> = {
     smoothScrollX: true,
@@ -164,58 +192,74 @@ function App() {
     isDraggable: false,
     rowMarkers: "none",
     width: "100%",
+    getCellsForSelection: true, // Allow copying
   };
 
   return (
-      <div className="App">
-        <h1>Transactions</h1>
-        <div className="filters">
-          <div>
-            <label>Start Date:</label>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-          </div>
-          <div>
-            <label>End Date:</label>
-            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
-          </div>
-          <div>
-            <label>Results Limit:</label>
-            <select value={limit} onChange={(e) => setLimit(parseInt(e.target.value))}>
-              <option value={10}>10</option>
-              <option value={50}>50</option>
-              <option value={100}>100</option>
-            </select>
-          </div>
-          <div>
-            <label>Transaction Natures:</label>
-            <Select
-                styles={customStyles}
-                options={trnNatureOptions}
-                isMulti
-                defaultValue={trnNatureOptions.find(option => option.value === DEFAULT_TRN_NATURE)}
-                onChange={handleTrnNatureChange}
-            />
-          </div>
-          <button onClick={() => fetchTransactions(1)}>Fetch Transactions</button>
-        </div>
-        <div className="grid-container dark-theme">
-          <DataEditor
-              theme={darkTheme}
-              {...gridProps}
-              columns={columns}
-              rows={trns.length}
-              getCellContent={getData}
-              rowHeight={35}
+    <div className="App">
+      <h1>Transactions</h1>
+      <div className="filters">
+        <div>
+          <label>Start Date:</label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
           />
         </div>
-        <div className="pagination">
-          <button disabled={page === 1} onClick={() => fetchTransactions(page - 1)}>
-            Previous
-          </button>
-          <span>Page {page}</span>
-          <button onClick={() => fetchTransactions(page + 1)}>Next</button>
+        <div>
+          <label>End Date:</label>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
         </div>
+        <div>
+          <label>Results Limit:</label>
+          <select
+            value={limit}
+            onChange={(e) => setLimit(parseInt(e.target.value))}
+          >
+            <option value={10}>10</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
+        </div>
+        <div>
+          <label>Transaction Natures:</label>
+          <Select
+            styles={customStyles}
+            options={trnNatureOptions}
+            isMulti
+            defaultValue={trnNatureOptions.find(
+              (option) => option.value === DEFAULT_TRN_NATURE
+            )}
+            onChange={handleTrnNatureChange}
+          />
+        </div>
+        <button onClick={() => fetchTransactions(1)}>Fetch Transactions</button>
       </div>
+      <div className="grid-container dark-theme">
+        <DataEditor
+          theme={darkTheme}
+          {...gridProps}
+          columns={columns}
+          rows={trns.length}
+          getCellContent={getData}
+          rowHeight={35}
+          onColumnResize={onColumnResize}
+        />
+      </div>
+      <div className="pagination">
+        <button
+          disabled={page === 1}
+          onClick={() => fetchTransactions(page - 1)}
+        >
+          Previous
+        </button>
+        <span>Page {page}</span>
+        <button onClick={() => fetchTransactions(page + 1)}>Next</button>
+      </div>
+    </div>
   );
 }
 
