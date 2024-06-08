f089724764b39377e73228e4a80debd2a8fc5f44
TomDD <scarletinne@hotmail.com>
2024-06-07 20:52:16 -0400

    Remove example logo & css

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -106,14 +104,6 @@ function App() {
 
   return (
       <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
         <h1>Transactions</h1>
         <div className="filters">
           <div>
