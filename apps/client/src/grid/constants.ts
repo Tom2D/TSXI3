0512f2c58b61b26230aaa5f0f5600c4cdbcc1430
TomDD <scarletinne@hotmail.com>
2024-06-13 23:36:22 -0400

    Numbers with ',' (using locale)

--- apps/client/src/grid/constants.ts
+++ apps/client/src/grid/constants.ts
@@ -1,5 +1,10 @@
-import { MRT_ColumnDef } from 'material-react-table';
+import { MRT_Cell, MRT_ColumnDef } from 'material-react-table';
 import { transactions } from '../prisma-types';
+
+const toLocaleNumber = (cell: MRT_Cell<any>): string | null => {
+  const value = cell.getValue<number>();
+  return value !== null ? value.toLocaleString() : null;
+};
 
 export const initialColumns: MRT_ColumnDef<transactions>[] = [
   { accessorKey: 'trnFlagId', header: 'Flag', size: 50 },
@@ -10,10 +15,28 @@ export const initialColumns: MRT_ColumnDef<transactions>[] = [
   { accessorKey: 'insider', header: 'Insider', size: 125 },
   { accessorKey: 'titles', header: 'Titles', size: 150 },
   { accessorKey: 'trnNatureCode', header: 'Trade Type', size: 200 },
-  { accessorKey: 'price', header: 'Price', size: 60 },
-  { accessorKey: 'nb', header: 'Quantity', size: 80 },
+  {
+    accessorKey: 'price',
+    header: 'Price',
+    size: 60,
+    Cell: ({ cell }) => toLocaleNumber(cell),
+  },
+  {
+    accessorKey: 'nb',
+    header: 'Quantity',
+    size: 80,
+    Cell: ({ cell }) => toLocaleNumber(cell),
+  },
   { accessorKey: 'securityId', header: 'Security Type', size: 150 },
-  { accessorKey: 'closingBalance', header: 'Balance', size: 80 },
+  {
+    accessorKey: 'closingBalance',
+    header: 'Balance',
+    size: 85,
+    Cell: ({ cell }) => toLocaleNumber(cell),
+    muiTableBodyCellProps: {
+      style: { whiteSpace: 'nowrap' }, // Disable text wrapping
+    },
+  },
   { accessorKey: 'ownershipType', header: 'Ownership', size: 90 },
   {
     accessorKey: 'GeneralRemarks',
