48c669946341741d96ff3a5fe7a99e3733193f88
TomDD <scarletinne@hotmail.com>
2024-06-18 01:29:34 -0400

    Red/Green value (if + or -)

--- apps/client/src/grid/columns-get.ts
+++ apps/client/src/grid/columns-get.ts
@@ -10,7 +10,7 @@ import {
   trnflag,
   securitydesignations,
 } from '../prisma-types';
-import { initialColumns } from './columns-def.ts';
+import { initialColumns } from './columns-def.tsx';
 
 export const columnsGet = (
   issuers: issuers[],
@@ -37,9 +37,7 @@ export const columnsGet = (
   };
 
   const getTitles = (insiderId: number): string => {
-    const relations = relationsToIssuer.filter(
-      (relation) => relation.insiderId === insiderId,
-    );
+    const relations = relationsToIssuer.filter((relation) => relation.insiderId === insiderId);
     return relations.map((relation) => relation.type).join(', ');
   };
 
@@ -72,8 +70,7 @@ export const columnsGet = (
 
       case 'trnDate':
       case 'filingDate':
-        accessorFn = (row) =>
-          new Date(row[column.accessorKey]).toLocaleDateString();
+        accessorFn = (row) => new Date(row[column.accessorKey]).toLocaleDateString();
         break;
 
       case 'trnNatureCode':
