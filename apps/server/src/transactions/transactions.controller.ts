24a2b7c68a8692330f2abe7f2e20b7058e4e7c9e
TomDD <scarletinne@hotmail.com>
2024-06-19 11:04:09 -0400

    Added insider&issuer fileter server side (WIP)

--- apps/server/src/transactions/transactions.controller.ts
+++ apps/server/src/transactions/transactions.controller.ts
@@ -15,9 +15,22 @@ export class TransactionsController {
     @Query('limit', new DefaultValuePipe(MAX_TRANSACTIONS_PER_REQUEST), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
     @Query('trnNatureCodes') trnNatureCodes: string,
+    @Query('issuerName') issuerName?: string,
+    @Query('insiderName') insiderName?: string,
+    @Query('insiderTitles') insiderTitles?: string,
   ): Promise<any> {
     const codes = trnNatureCodes ? trnNatureCodes.split(',').map(Number) : [];
-    return this.transactionsService.findAll(beginFilingDate, endFilingDate, limit, page, codes);
+    const titles = insiderTitles ? insiderTitles.split(',') : [];
+    return this.transactionsService.findAll(
+      beginFilingDate,
+      endFilingDate,
+      limit,
+      page,
+      codes,
+      issuerName,
+      insiderName,
+      titles,
+    );
   }
 
   @Get(':id')
