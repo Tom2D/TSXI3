bbe70328c2dfefd9079c329421680b048db6a5f5
TomDD <scarletinne@hotmail.com>
2024-06-29 01:47:11 -0400

    Added log for prisma underlying sql queries

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -1,9 +1,28 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+const PRISMA_LOGGING = false;
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
   async onModuleInit() {
+    this.$on('query' as never, (e: any) => {
+      if (PRISMA_LOGGING) {
+        console.log(`${e.query} ${e.params}`);
+      }
+    });
+
     await this.$connect();
   }
 }
