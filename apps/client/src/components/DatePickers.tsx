6b220f363b0efa8483041f39e6e9b0f6a07ae244
TomDD <scarletinne@hotmail.com>
2024-07-01 00:14:50 -0400

    Updated date format in pickers

--- apps/client/src/components/DatePickers.tsx
+++ apps/client/src/components/DatePickers.tsx
@@ -1,6 +1,7 @@
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Dayjs } from 'dayjs';
+import { DATE_FORMAT } from '../util/date.ts';
 
 interface Props {
   startDate: Dayjs | null;
@@ -9,12 +10,7 @@ interface Props {
   setEndDate: (date: Dayjs | null) => void;
 }
 
-const DatePickers: React.FC<Props> = ({
-  startDate,
-  endDate,
-  setStartDate,
-  setEndDate,
-}) => (
+const DatePickers: React.FC<Props> = ({ startDate, endDate, setStartDate, setEndDate }) => (
   <LocalizationProvider dateAdapter={AdapterDayjs}>
     <div>
       <DatePicker
@@ -22,12 +18,14 @@ const DatePickers: React.FC<Props> = ({
         value={startDate}
         onChange={(newDate) => setStartDate(newDate)}
         slotProps={{ field: { clearable: true } }}
+        format={DATE_FORMAT}
       />
       <DatePicker
         label="End Date"
         value={endDate}
         onChange={(newDate) => setEndDate(newDate)}
         slotProps={{ field: { clearable: true } }}
+        format={DATE_FORMAT}
       />
     </div>
   </LocalizationProvider>
--- apps/client/src/util/date.ts
+++ apps/client/src/util/date.ts
@@ -1,5 +1,7 @@
 import { Dayjs } from 'dayjs';
 
+export const DATE_FORMAT = 'YYYY-MM-DD';
+
 // Convert date to string in YYYY-MM-DD format (UTC timezone)
 export const FormatDateUTC = (date: Dayjs | null): string => {
   return date ? date.toISOString().split('T')[0] : '';
